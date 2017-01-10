b9ec7ddaa45b82cf79bd76fc1558d246bc0fe095
Ian Reid <ian@anyuzer.com>
2017-01-09 17:13:50 -0800

    x

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 global.APP_PATH = require('path').resolve(__dirname+"/src")
 global.TLS_PATH = require('path').resolve(__dirname+"/tls");
 
-require('./src/Main').Run();
+require('./src/Main').Run(); 
